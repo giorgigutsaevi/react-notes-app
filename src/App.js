d993e4a124ceca4a033651bd79a4c41dd4dc6ef0
Giorgi Gutsaev <giorgi.gutsaevi@gmail.com>
2022-03-15 10:19:21 +0000

    setup state for notes

--- src/App.js
+++ src/App.js
@@ -3,9 +3,14 @@ import {nanoid} from 'nanoid'
 import NotesList from "./components/NotesList";
 
 const App = () => {
+  
+  const today = new Date();
+  const date = `${today.getDate()}-${today.getMonth() + 1}-${today.getFullYear()}`
 
   const [notes, setNotes] = useState([{
     id: nanoid(),
+    text: "Sample note text",
+    date: date,
   }]);
 
   return (
