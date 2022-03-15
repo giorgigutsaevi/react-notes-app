e2c91d7fa0eb1959ab641b48128e8f15db73a7a4
Giorgi Gutsaev <giorgi.gutsaevi@gmail.com>
2022-03-15 10:56:18 +0000

    added onChange event to the textarea input element to listen to changes as the user types a new note

--- src/components/AddNote.js
+++ src/components/AddNote.js
@@ -1,8 +1,21 @@
+import { useState } from "react";
+
 const AddNote = () => {
+	const [noteText, setNoteText] = useState("");
+
+	const handleChange = (evt) => {
+		setNoteText(evt.target.value);
+	}
+
 	return (
 		<div className='note new'>
-			<textarea 
-				rows='8' cols='10' placeholder="Enter your new note...">
+			<textarea
+					rows='8' 
+					cols='10' 
+					placeholder="Enter your new note..."
+					onChange={handleChange}
+					value={noteText}
+				>
 			</textarea>
 			<div className='note-footer'>
 				<small>200 Remaining</small>
