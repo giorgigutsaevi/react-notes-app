d6758b041579558cda42ee64f9037df256ed9198
Giorgi Gutsaev <giorgi.gutsaevi@gmail.com>
2022-03-15 12:30:06 +0000

    added styles to my Header component

---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+import logo from "../images/logo.png"
+
+const Header = () => {
+	return (
+		<div className='notes-header'>
+			<div className='logo-wrapper'>
+				<img className='app-logo' src={logo} alt='react-logo'></img>
+				<h1>React Notes</h1>
+			</div>
+			<button className='toggle-mode-button'>Dark Mode</button>
+		</div>
+	)
+}
+
+export default Header;
