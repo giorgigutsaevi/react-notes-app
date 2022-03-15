35b1be8adfbaf903d4bb2e81b7042607572a20b5
Giorgi Gutsaev <giorgi.gutsaevi@gmail.com>
2022-03-15 10:14:20 +0000

    install nanoid npm package and import useState hook from react

--- src/App.js
+++ src/App.js
@@ -1,6 +1,13 @@
+import { useState } from "react";
+import {nanoid} from 'nanoid'
 import NotesList from "./components/NotesList";
 
 const App = () => {
+
+  const [notes, setNotes] = useState([{
+    id: nanoid(),
+  }]);
+
   return (
   <div className='container'>
     <NotesList />
